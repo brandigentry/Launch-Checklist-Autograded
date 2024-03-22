b3d0de72798c897dd4600a71b1472a4b8acb0940
Brandi Gentry <brandigentry@Brandis-MacBook-Air.local>
2024-03-21 20:36:15 -0500

    Script.js error unsolved.

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 // Write your JavaScript code here!
 
-const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
+// const { myFetch, pickPlanet, addDestinationInfo, formSubmission } = require("./scriptHelper");
 
-window.addEventListener("load", function() {
-    const button = document.getElementById("formSubmit");
-    button.addEventListener("click", function(event){
-        event.preventDefault();
-        let pilot = document.querySelector("input[name=pilotName]");
-        let copilot = document.querySelector("input[name=copilotName]");
-        let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        let cargoLevel = document.querySelector("input[name=cargoMass]");
-        let list = document.getElementById('faultyItems');
+// window.addEventListener("load", function() {
+//     const button = document.getElementById("formSubmit");
+//     button.addEventListener("click", function(event){
+//         event.preventDefault();
+//         let pilot = document.querySelector("input[name=pilotName]");
+//         let copilot = document.querySelector("input[name=copilotName]");
+//         let fuelLevel = document.querySelector("input[name=fuelLevel]");
+//         let cargoLevel = document.querySelector("input[name=cargoMass]");
+//         let list = document.getElementById('faultyItems');
 
-        formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
-    });
+//         formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value);
+//     });
     
+//Add an event handler for the window load event
+window.addEventListener("load", function() {
+
+    // TASK 2
+    //Within the window’s load handler, add an event handler for the form submit event
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        //prevent the form submission from happening until all inputs have valid values
+        event.preventDefault();
+
+        //Retrieve input values that need to be validated from the DOM.
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoMass = document.querySelector("input[name=cargoMass]").value;
+        let list = document.getElementById("faultyItems");
+
+        //Make sure to call your formSubmission() function at the appropriate time in your script.js file!
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+    })
+
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     listedPlanets = myFetch();
